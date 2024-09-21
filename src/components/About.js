90d4cde51ef0e55aa71aa6527c46418a4c194829
Harsh Bisht <168906728+harshbisht27@users.noreply.github.com>
2024-09-21 20:08:25 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -71,9 +71,9 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="about-box">
-        <h1 className="about-heading">All About Veda Vibes</h1>
+        <h1 className="about-heading">All About AyurSphere</h1>
         <p>
-          Veda Vibes is an Ayurvedic platform designed to promote the ancient wisdom of Ayurveda, especially among youth. Our mission is to make Ayurveda more accessible and relatable to the modern generation by blending traditional knowledge with contemporary lifestyles. At Veda Vibes, you can explore the principles of Ayurveda, natural remedies, holistic healing, and yoga practices tailored to your individual health needs. Join us in a journey towards balanced living, healthy choices, and a better understanding of how Ayurveda can enhance your life.
+          AyurSphere is an Ayurvedic platform designed to promote the ancient wisdom of Ayurveda, especially among youth. Our mission is to make Ayurveda more accessible and relatable to the modern generation by blending traditional knowledge with contemporary lifestyles. At AyurSphere, you can explore the principles of Ayurveda, natural remedies, holistic healing, and yoga practices tailored to your individual health needs. Join us in a journey towards balanced living, healthy choices, and a better understanding of how Ayurveda can enhance your life.
         </p>
 
         <div className="carousel-container">
